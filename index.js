1dbb68e9b586dd8a9367300e89ca842ee30913e8
nahashon <nashonmwendwa0@gmail.com>
2024-07-12 21:34:14 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+// index.js
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+  };
+  
+  function wrapAdjective(flair = '*') {
+    return function(adjective = 'pecial') {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
